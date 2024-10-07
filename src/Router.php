d95513da4e534b4be4c974fac7b353f23a1499cb
Jacques-Antoine <jacques.antoine.dev@gmail.com>
2024-10-07 20:42:28 +0200

    Initial commit

---
+++ src/Router.php
@@ -0,0 +1,40 @@
+<?php
+
+enum HttpMethod: string {
+    case GET = 'GET';
+    case POST = 'POST';
+    case PUT = 'PUT';
+    case DELETE = 'DELETE';
+    case PATCH = 'PATCH';
+}
+
+class Router {
+    private array $routes = [];
+
+    public function addRoute(HttpMethod $method, string $path, callable $callback): void {
+        $this->routes[] = [
+            'method' => $method,
+            'path' => $path,
+            'callback' => $callback
+        ];
+    }
+
+    public function dispatch(string $requestUri, string $requestMethod): void {
+        $httpMethod = HttpMethod::tryFrom($requestMethod);
+        if (!$httpMethod) {
+            http_response_code(405); // 405 Method Not Allowed
+            echo "405 Method Not Allowed";
+            return;
+        }
+
+        foreach ($this->routes as $route) {
+            if ($route['path'] === $requestUri && $route['method'] === $httpMethod) {
+                $route['callback'](); // First-class callable, mais pas de return nécessaire
+                return;
+            }
+        }
+
+        http_response_code(404);
+        echo "404 Not Found";
+    }
+}
