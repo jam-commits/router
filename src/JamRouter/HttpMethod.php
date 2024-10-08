3b52f06910018bc63eb0e93bc9f6eef2785f4577
Jacques-Antoine <jacques.antoine.dev@gmail.com>
2024-10-08 23:17:08 +0200

    Ajout namespace

---
+++ src/JamRouter/HttpMethod.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JamRouter\Router;
+
+enum HttpMethod: string {
+case GET = 'GET';
+case POST = 'POST';
+case PUT = 'PUT';
+case DELETE = 'DELETE';
+case PATCH = 'PATCH';
+}
--- src/JamRouter/Router.php
+++ src/JamRouter/Router.php
@@ -1,12 +1,6 @@
 <?php
 
-enum HttpMethod: string {
-    case GET = 'GET';
-    case POST = 'POST';
-    case PUT = 'PUT';
-    case DELETE = 'DELETE';
-    case PATCH = 'PATCH';
-}
+namespace JamRouter\Router;
 
 class Router {
     private array $routes = [];
