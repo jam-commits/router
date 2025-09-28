988a7bbc6aa9cd957a49c265a7107b1780de2dde
Jacques-Antoine <jacques.antoine.dev@gmail.com>
2025-09-28 21:06:02 +0200

    add docblocks

--- src/Router.php
+++ src/Router.php
@@ -5,6 +5,12 @@ namespace JamCommits\Router;
 class Router {
     private array $routes = [];
 
+    /**
+    * Function to add a route handled by the router.
+    * @param HttpMethod $method It is the compatible http verb with the route
+    * @Param string $path It is the path of the route ex "login" for http://domaine.fr/login
+    * @Param callable $callback It is the callback executed when the route is reached.
+    */
     public function addRoute(HttpMethod $method, string $path, callable $callback): void {
         $this->routes[] = [
             'method' => $method,
@@ -13,6 +19,12 @@ class Router {
         ];
     }
 
+
+    /**
+    * Function that match route with uri and method and execute associated callback.
+    * @param $requestUri request uri of the http request
+    * @param $requestMethod verb of the http request
+    */
     public function dispatch(string $requestUri, string $requestMethod): void {
         $httpMethod = HttpMethod::tryFrom($requestMethod);
         if (!$httpMethod) {
